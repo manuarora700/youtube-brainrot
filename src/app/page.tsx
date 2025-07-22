6d5d3795b302ce32a7f1dd9a88a358dbfdfd3154
Manu Arora <manuarorawork@gmail.com>
2025-07-22 12:46:04 +0530

    progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Founders } from "@/components/founders";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex items-center justify-center"></div>
+    <div className="h-screen w-full flex items-center justify-center">
+      <Founders />
+    </div>
   );
 }
