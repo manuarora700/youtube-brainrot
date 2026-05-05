f706e17b77a2cff1e3217c49b5489cca4f4f5d61
Manu Arora <manuarorawork@gmail.com>
2026-05-05 14:04:34 +0530

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,15 @@ import {
   Delius,
   Geist,
   Geist_Mono,
-  Inter,
+  Inter, Roboto_Slab, Manrope,
+  Schibsted_Grotesk
 } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+
+const robotoSlab = Roboto_Slab({ subsets: ['latin'], variable: '--font-serif' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,13 +34,19 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
+const schibsted = Schibsted_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-schibsted",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn(robotoSlab.variable, "font-sans", geist.variable, schibsted.variable)}>
       <body className={`${inter.className}  antialiased`}>{children}</body>
     </html>
   );
